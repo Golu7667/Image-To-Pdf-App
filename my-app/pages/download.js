4a8f0b0c470ea6ab3f06825e4e489325b33ac0c7
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-08 08:22:50 +0000

    add pages

---
+++ my-app/pages/download.js
@@ -0,0 +1,14 @@
+
+export default Download=()=>{
+    reurn (
+        <div>
+        <h2>Your PDF is ready!</h2>
+        <iframe
+          src={pdf.output('bloburl')}
+          width="100%"
+          height="600px"
+          title="Converted PDF"
+        ></iframe>
+      </div>
+    )
+}
---
+++ my-app/pages/index.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import jsPDF from 'jspdf';
+
+
+export default Home=()=>{
+    const [images, setImages] = useState([]);
+    const [pdf, setPdf] = useState(null);
+    const [check,setCheck]=useState(false)
+  
+    const handleFileChange = (e) => {
+      const selectedImages = Array.from(e.target.files);
+      setImages(selectedImages);
+    };
+  
+    const generatePDF = () => {
+      const doc = new jsPDF();
+      images.forEach((image, index) => {
+        if (index !== 0) {
+          doc.addPage();
+        }
+        const reader = new FileReader();
+        reader.readAsDataURL(image);
+        reader.onload = () => {
+          doc.addImage(reader.result, 'JPEG', 10, 10, 190, 0);
+          if (index === images.length - 1) {
+            setPdf(doc);
+          }
+        };
+      });
+      setCheck(true)
+    };
+  
+    const downloadPDF = () => {
+      if (pdf) {
+        pdf.save('converted.pdf');
+      }
+    };
+  
+
+    return (
+    <div>
+    <h1>Image to PDF Converter</h1>
+    <input type="file" accept=".jpg, .jpeg, .png" multiple onChange={handleFileChange} />
+    <button onClick={generatePDF}>Convert to PDF</button>
+    
+    </div>
+    )
+}
